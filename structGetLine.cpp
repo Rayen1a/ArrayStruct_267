544c3de959666539be468056502bf55eb59e5731
Rayen1a <raihan01426@gmail.com>
2025-04-15 11:44:55 +0700

    memasukkan data mahasiswa

--- structGetLine.cpp
+++ structGetLine.cpp
@@ -14,3 +14,16 @@ struct mahasiswa
     string nama;
     detailAlamat alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cin.ignore();
+    cout << "Masukkan NAMA = ";
+    getline(cin , mhs.nama);
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
