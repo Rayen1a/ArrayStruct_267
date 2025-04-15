49e7849c2561c766a4e2c3bd0287e4645e8ade49
Rayen1a <raihan01426@gmail.com>
2025-04-15 11:40:32 +0700

    memasukkan data diri mahasiswa

--- struct2.cpp
+++ struct2.cpp
@@ -14,3 +14,15 @@ struct mahasiswa
     string nama;
     detailAlamat alamat;
 };
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan NAMA = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
